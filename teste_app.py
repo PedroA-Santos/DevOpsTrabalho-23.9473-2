94abe21aa4998aa5ce5caac9805eb8faefe5386a
Pedro Antonio <eupedroassuncao@gmail.com>
2024-11-24 21:37:21 -0300

    arrumando dokcer

--- teste_app.py
+++ teste_app.py
@@ -1,12 +1,9 @@
-# test_app.py
-
 import pytest
 from app import app, db
 from flask import json
 
 @pytest.fixture
 def client():
-    # Configuração da aplicação para testes
     app.config['TESTING'] = True
     app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://flask_user:flask_password@mariadb/school_db'
     with app.test_client() as client:
@@ -17,14 +14,12 @@ def client():
 def test_listar_alunos(client):
     response = client.get('/alunos')
     assert response.status_code == 200
-    assert isinstance(response.json, list)  # Verifica se a resposta é uma lista
+    assert isinstance(response.json, list)
 
 def test_adicionar_aluno(client):
     aluno = {
         'nome': 'João',
-        'sobrenome': 'Silva',
-        'turma': '5B',
-        'disciplinas': 'Matemática, Português'
+        'ra': '12345'
     }
     response = client.post('/alunos', data=json.dumps(aluno), content_type='application/json')
     assert response.status_code == 201
